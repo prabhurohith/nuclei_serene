7e98b751e5251b0230d831ceffbd96ddbade6290
rohith prabhu <prabhurohith@gmail.com>
2023-03-22 11:38:28 +0530

    Adding reporting capabilities

--- src/test/java/starter/stepdefinitions/Hooks.java
+++ src/test/java/starter/stepdefinitions/Hooks.java
@@ -2,15 +2,20 @@ package starter.stepdefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 import questions.EnvironmentInfo;
 
 public class Hooks {
 
+    @BeforeAll
+    public static void setupReporting() {
+        configureReporting();
+    }
+
     @Before
     public void setTheStage() {
-
         if (EnvironmentInfo.getRunProfile().toLowerCase().contains("web")) {
             OnStage.setTheStage(new OnlineCast());
 //            OnStage.theActorInTheSpotlight().abilityTo(BrowseTheWeb.class);
@@ -19,14 +24,15 @@ public class Hooks {
             OnStage.setTheStage(new OnlineCast());
         }
     }
-/*    @Before
-    public void setTheStage() {
-        OnStage.setTheStage(new OnlineCast());
-    }*/
 
     @After
     public void cleanTheStage() {
         OnStage.drawTheCurtain();
+    }
+
+    private static void configureReporting() {
+        System.setProperty("rp.attributes", "platform:" + EnvironmentInfo.getRunProfile().toLowerCase());
+        System.setProperty("rp.launch", "Test_" + EnvironmentInfo.getRunProfile().toLowerCase());
     }
 
 }
